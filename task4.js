9a00995982cf18609ccd7e15a44b5e9de33df305
Luanderson almeida <92821001+LuandersonASantos@users.noreply.github.com>
2022-12-07 14:05:45 -0300

    Task 4: Fetch neighboring countries

--- task4.js
+++ task4.js
@@ -3,13 +3,28 @@
 const fetchCountry = async (alpha3Code) => {
   try {
     const res = await fetch(
-      `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`
+      `https://restcountries.com/v3.1/alpha/${alpha3Code}`
     );
 
     const data = await res.json();
 
-    return data;
+    return data[0];
   } catch (error) {
     console.log(error);
   }
 };
+
+const fetchCountryAndNeigbors = async () =>{
+  const columbia = await fetchCountry("col");
+
+  const neighbors = await Promise.all( 
+    /*columbia.forEach(element => {
+      console.log(element.borders, "teste")
+      
+    })*/
+    columbia.borders.map((border)=> fetchCountry(border))
+    );
+  console.log(neighbors);
+}
+
+fetchCountryAndNeigbors();
